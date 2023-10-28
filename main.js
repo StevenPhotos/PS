b1a24a5bbe944f0bf3d9e33ca30d6a550afbba3a
Steven <buildpucipuci02@gmail.com>
2023-10-28 18:06:29 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+const gainNode = audioCtx.createGain();
+const playButton = document.getElementById('playBTN');
+let shifter;
+
+const fileInput = document.getElementById('fileInput'); // Assuming you have an input element of type 'file' in your HTML
+
+fileInput.addEventListener('change', function (event) {
+  const file = event.target.files[0];
+  const reader = new FileReader();
+
+  reader.onload = function (e) {
+    const audioData = e.target.result;
+
+    if (shifter) {
+      shifter.off(); // remove any current listeners
+    }
+
+    audioCtx.decodeAudioData(audioData, (audioBuffer) => {
+      shifter = new PitchShifter(audioCtx, audioBuffer, 1024);
+      shifter.on('play', (detail) => {
+        // Handle play event details here
+        // console.log(detail.timePlayed);
+        // console.log(detail.formattedTimePlayed);
+        // console.log(detail.percentagePlayed);
+      });
+      shifter.tempo = 0.9; // Set the initial tempo
+      // shifter.pitch = 1; // Set the initial pitch
+      // shifter.rate = 0.5;
+    });
+
+    // play();
+  };
+
+  reader.readAsArrayBuffer(file);
+});
+
+playButton.addEventListener('click', ()=>{
+  // console.log(shifter.pitch.value);
+  shifter.pitch = 0.8;
+  play();
+});
+
+function play() {
+  if (shifter) {
+    shifter.connect(gainNode); // Connect it to a GainNode to control the volume
+    gainNode.connect(audioCtx.destination); // Attach the GainNode to the 'destination' to begin playback
+  } else {
+    console.error('No audio file loaded yet.');
+  }
+};
