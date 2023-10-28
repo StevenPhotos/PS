127e228c90e036e7d022282880752afeefee1474
Steven <buildpucipuci02@gmail.com>
2023-10-28 18:25:20 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,42 +1,51 @@
 const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 const gainNode = audioCtx.createGain();
 const playButton = document.getElementById('playBTN');
+const fileInput = document.getElementById('fileInput'); // Assuming you have an input element of type 'file' in your HTML
 let shifter;
 
-const fileInput = document.getElementById('fileInput'); // Assuming you have an input element of type 'file' in your HTML
 
-fileInput.addEventListener('change', function (event) {
-  const file = event.target.files[0];
-  const reader = new FileReader();
+fileInput.addEventListener('change', async (e) => {
+  // if(shifter){
+  // }
 
-  reader.onload = function (e) {
-    const audioData = e.target.result;
+  const file = e.target.files[0];
+  if (!file) return;
 
-    if (shifter) {
-      shifter.off(); // remove any current listeners
-    }
+  // const reader = new FileReader();
 
-    audioCtx.decodeAudioData(audioData, (audioBuffer) => {
-      shifter = new PitchShifter(audioCtx, audioBuffer, 1024);
-      shifter.on('play', (detail) => {
-        // Handle play event details here
-        // console.log(detail.timePlayed);
-        // console.log(detail.formattedTimePlayed);
-        // console.log(detail.percentagePlayed);
-      });
-      shifter.tempo = 0.9; // Set the initial tempo
-      // shifter.pitch = 1; // Set the initial pitch
-      // shifter.rate = 0.5;
+  // reader.onload = function (e) {
+  //   const audioData = e.target.result;
+  // };
+
+  // if (shifter) {
+  //   shifter.off(); // remove any current listeners
+  // }
+
+  const response = await fetch(URL.createObjectURL(file));
+  const audioData = await response.arrayBuffer();
+    
+
+  audioCtx.decodeAudioData(audioData, (audioBuffer) => {
+    shifter = new PitchShifter(audioCtx, audioBuffer, 1024);
+    shifter.on('play', (detail) => {
+      // Handle play event details here
+      // console.log(detail.timePlayed);
+      // console.log(detail.formattedTimePlayed);
+      // console.log(detail.percentagePlayed);
     });
+    shifter.tempo = 0.9; // Set the initial tempo
+    // shifter.pitch = 1; // Set the initial pitch
+    // shifter.rate = 0.5;
+  });
 
     // play();
-  };
 
-  reader.readAsArrayBuffer(file);
+
+  // reader.readAsArrayBuffer(file);
 });
 
 playButton.addEventListener('click', ()=>{
-  // console.log(shifter.pitch.value);
   shifter.pitch = 0.8;
   play();
 });
